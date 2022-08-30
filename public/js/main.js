a5dc5fb9818ddcd3b3e8a6ed835c54e1922471bb
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-08-30 16:24:52 -0700

    Boilerplate html, css, js for pixiJS

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+let type = "WebGL";
+if (!PIXI.utils.isWebGLSupported()) type = "canvas";
+PIXI.utils.sayHello(type);
+
+const app = new PIXI.Application({width: 256, height: 256});
+document.body.appendChild(app.view);
