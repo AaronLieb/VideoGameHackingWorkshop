7ac8521e48d7087e4990fde7959a35be36293260
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-08-30 16:25:39 -0700

    basic web server for static files

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
 
-const port = 8080;
+const PORT = 80;
 
 const handler = (request: Request): Response => {
-    const body = Deno.readFileSync("./public/index.html");
+    let { pathname } = new URL(request.url);
+
+    if (pathname === "/") pathname = "/public/index.html";
+    pathname = `.${pathname}`;
+    const body = Deno.readFileSync(pathname);
     return new Response(body, { status: 200 });
 };
 
-console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
-await serve(handler, { port });
+console.log(`HTTP webserver running. Access it at: http://localhost:${PORT}/`);
+await serve(handler, { PORT });
