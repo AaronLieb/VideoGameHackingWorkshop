4888d1468bc77fa2b47da4da263fbfdc410ae51b
diamondburned <diamond@arikawa-hi.me>
2022-08-30 20:45:28 -0700

    Add src/deps.ts

---
+++ src/deps.ts
@@ -0,0 +1,1 @@
+export * as server from "https://deno.land/std@0.152.0/http/server.ts";
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+import { server } from "./deps.ts";
 
-const PORT = 80;
+const port = 80;
 
 const handler = (request: Request): Response => {
     let { pathname } = new URL(request.url);
@@ -11,5 +11,5 @@ const handler = (request: Request): Response => {
     return new Response(body, { status: 200 });
 };
 
-console.log(`HTTP webserver running. Access it at: http://localhost:${PORT}/`);
-await serve(handler, { PORT });
+console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
+await server.serve(handler, { port });
