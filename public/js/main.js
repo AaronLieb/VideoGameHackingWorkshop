3e902f17958d1840956c6141b3c1906329a951a5
diamondburned <diamond@arikawa-hi.me>
2022-09-10 06:54:54 -0700

    Add login page, use picoCSS, WS working

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,22 @@
-import * as ws from "/public/js/common/wsclient.js";
 import * as session from "/public/js/session.js";
+
+const ses = await session.Connect()
+    .catch((err) => {
+        if (err instanceof session.NotLoggedInError) {
+            window.location.replace("/login/");
+        } else {
+            alert(`cannot connect to WS: ${err}`);
+        }
+        throw err;
+    });
+
+console.log(`logged in as ${ses.username}`);
 
 let type = "WebGL";
 if (!PIXI.utils.isWebGLSupported()) type = "canvas";
 PIXI.utils.sayHello(type);
 
-const app = new PIXI.Application({ width: 256, height: 256 });
-document.body.appendChild(app.view);
-
-const ses = session.Connect();
+// 640x400 has 400 which is a multiple of 16. We also want the viewport to be
+// 16:10.
+const app = new PIXI.Application({ width: 640, height: 400 });
+document.getElementById("game").appendChild(app.view);
--- public/js/session.js
+++ public/js/session.js
@@ -1,25 +1,70 @@
 import * as map from "/public/js/common/map.js";
 import * as ws from "/public/js/common/wsclient.js";
 
-export function Connect(url = "ws:///api/ws") {
-    const socket = new WebSocket(url);
+export class NotLoggedInError extends Error {
+    constructor() {
+        super("user not logged in");
+    }
+}
+
+// Connect ensures that the user is authorized then returns a new Session that's
+// connected to the Websocket.
+export async function Connect() {
+    // Check if we're authenticated.
+    const auth = await fetch("/api/auth");
+    if (auth.status != 200) {
+        throw new NotLoggedInError();
+    }
+
+    const host = location.host;
+    if (!host) throw "missing location.lost";
+
     const session = new Session();
-    const wrapped = new ws.Client(socket, session);
-    return wrapped;
+
+    // waitHello blocks until either we get a HELLO event or the Websocket
+    // unexpectedly closes.
+    // TODO: really refactor this. Not sure how. We don't have Go-style
+    // channels.
+    const waitHello = new Promise((resolve, reject) => {
+        const handler = (_ws, ev) => {
+            switch (ev.type) {
+                case "HELLO":
+                    resolve();
+                    break;
+                case "_close":
+                    reject("Websocket unexpectedly closed");
+                    break;
+                default:
+                    return;
+            }
+            session.hooks.delete(handler);
+        };
+        session.hooks.add(handler);
+    });
+
+    const socket = new WebSocket(`ws://${host}/api/ws`);
+    new ws.Client(socket, session);
+
+    await waitHello;
+    return session;
 }
 
 export class Session {
+    username;
     state;
+    hooks; // Set<(ws, ev) => void>
     maps; // Map<int, map.Map>
 
     constructor() {
         this.state = {};
+        this.hooks = new Set();
         this.maps = new Map();
     }
 
-    handleEvent(_ws, ev) {
+    handleEvent(ws, ev) {
         switch (ev.type) {
             case "HELLO": {
+                this.username = ev.d.username;
                 this.state.nLevels = ev.d.nLevels;
                 this.state.completedLevels = ev.d.completedLevels;
                 break;
@@ -38,15 +83,11 @@ export class Session {
                 break;
             }
         }
+
+        for (const fn of this.hooks) {
+            fn(ws, ev);
+        }
     }
-}
-
-export function isAuthorized() {
-    return !!hasCookie("VGHW-Username"); // :^)
-}
-
-function hasCookie(name) {
-    return document.cookie.match(`(?:^|; )${name}=`) != null;
 }
 
 export class Level {
